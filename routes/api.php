61cc5ce0607e7807b2182e374a7ff2b263bf4446
Evi Nirmalasari <evinirmalasarisari@gmail.com>
2022-10-24 07:58:25 +0700

    crud siswa

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\HeloController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//})
+
+/*Task
+*URL :  http://localhost:8000/api/halo
+*METHOD: GET
+*Exec: function
+*Return : JSON => {"me": "Cantik"}
+*/
+
+Route::get('halo', function(){
+    $data = ["me" => "Cantik"];
+
+    return $data;
 });
+
+//Route::get('siswa',[SiswaController::class, 'index']);
+//Route::get('siswa/{id}',[SiswaController::class, 'show']);
+//Route::post('siswa',[SiswaController::class, 'store']);
+//Route::get('siswa/{id}',[SiswaController::class, 'update']);
+//Route::get('siswa/{id}',[SiswaController::class, 'destroy']);
+
+Route::resource('siswa', SiswaController::class);
+
+
